6ef700267be41438a30f65383010923d2cda7e7e
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2025-02-21 13:55:46 +0530

    added user login and register backend

--- backend/controllers/UserControllers.js
+++ backend/controllers/UserControllers.js
@@ -1,24 +1,28 @@
 import User from "../models/User.js";
+import bcryptjs from "bcryptjs";
+
+const salt = bcryptjs.genSaltSync(10);
 
 export const Register = async (req, res) => {
   const { name, email, password } = req.body;
-//   console.log(name, email, password);
+  //   console.log(name, email, password);
   if (!name && !email && !password) {
     res.status(422).json({ message: "fields cannot be emplty" });
   }
 
-  let findUser =await User.findOne({email});
-  if(findUser){
+  let findUser = await User.findOne({ email });
+  if (findUser) {
     res.status(400).json({ message: "users already exists" });
     return;
   }
 
   let users;
+  const hashedpass = bcryptjs.hashSync(password, salt);
   try {
     users = new User({
       name,
       email,
-      password,
+      password: hashedpass,
     });
     await users.save();
   } catch (err) {
@@ -32,4 +36,27 @@ export const Register = async (req, res) => {
   res
     .status(200)
     .json({ success: true, message: "data added successfully", users });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email && !password) {
+    res.status(422).json({ message: "fileds cannot be empty" });
+  }
+  let user;
+  try {
+    user = await User.findOne({ email });
+    console.log(user.password);
+  } catch (err) {
+    console.log(err);
+  }
+  const comparePass = bcryptjs.compareSync(password, user.password);
+  const success=false;
+  if (!comparePass) {
+    res.status(400).json({ success,message: "invalid password" });
+  } else {
+    res
+      .status(200)
+      .json({ success: "true", message: "login successfull", user });
+  }
 };
--- backend/routers/UserRoutes.js
+++ backend/routers/UserRoutes.js
@@ -1,6 +1,7 @@
-import { Register } from "../controllers/UserControllers.js";
+import { login, Register } from "../controllers/UserControllers.js";
 import express from "express";
 const userRouter = express.Router();
 userRouter.get("/register",Register);
+userRouter.get("/login",login);
 
 export default userRouter;
